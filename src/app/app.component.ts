181715503f8da32fe6a79b767559a57821d384ec
Pratik Srivastav <pratik.srivastav@edufied.edu.vn>
2020-08-12 15:55:21 +0530

    changes commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'codeBlock';
+  title = 'CodeBlock';
+  url;
+  demoArray = new Array(6).fill('');
+
+  processFile(event: any, index){
+    if(event.target.files && event.target.files[0]){
+      var reader = new FileReader();
+      reader.onload = () => {
+        this.url = reader.result as string;
+        this.demoArray[index] = this.url;
+          for(let i=0; i<this.demoArray.length; i++){
+              if(this.demoArray[i] == '' && i<index){
+                this.demoArray[i] = this.demoArray[index];
+                this.demoArray[index] = '';
+              }
+          }        
+      }   
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+
+  deleteImg(index){
+    this.demoArray.splice(index, 1);
+    this.demoArray.push('');
+  }
 }
